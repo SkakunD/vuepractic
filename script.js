8f9f0e8ef50a201b4a4fdc2ed562831440ff8857
SkakunD <beenom314@gmail.com>
2021-02-11 00:32:32 +0300

    add function replace "li" and count new task

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ let numCompleteList = 0;
 let dataTask = document.querySelector(".input_data_task");
 let btnAddTask = document.querySelector(".btn_add_task");
 let dataItemsTodo = document.querySelector(".mask-list-todo");
+let dataItemCompleteList = document.querySelector(".complete-list");
 
 let spanNumDoList = document.querySelector(".mask-num-todo");
 let spanNumCompleteList = document.querySelector(".mask-num-done");
+
+let checkboxList;
 
 spanNumDoList.innerHTML = numDoList;
 spanNumCompleteList.innerHTML = numCompleteList;
@@ -14,15 +17,46 @@ spanNumCompleteList.innerHTML = numCompleteList;
 btnAddTask.onclick = function () {
   let dataItemTodoList = document.createElement("li");
   let dataItemTodo = dataTask.value;
+  dataItemTodoList.className = "inData";
   dataItemTodoList.innerHTML = `
-  <div>
-    <input type="checkbox" />
+  <div class="parent">
+    <input type="checkbox" onchange = replace() />
     <span class="data_todo_item">${dataItemTodo}</span>
   </div>
   <button class="btn-remove">Remove</button>
 `;
   dataItemsTodo.append(dataItemTodoList);
+
+  numDoList = document.querySelectorAll(".inData").length;
+  spanNumDoList.innerHTML = numDoList;
   dataTask.value = "";
+  checkboxList = document.querySelectorAll("input[type=checkbox]");
 };
-console.log(dataTask);
-console.log(btnAddTask);
+
+let replaceItem;
+function replace() {
+  for (let i = 0; i < checkboxList.length; i++) {
+    if (checkboxList[i].checked) {
+      replaceItem = checkboxList[i].parentNode.parentNode;
+      numDoList -= 1;
+      spanNumDoList.innerHTML = numDoList;
+      numCompleteList += 1;
+      spanNumCompleteList.innerHTML = numCompleteList;
+      // console.log(replaceItem);
+      // console.log(dataItemCompleteList);
+      dataItemCompleteList.append(replaceItem);
+      // console.log(checkboxList[i].parentNode.parentNode);
+    } else if (!checkboxList[i].checked) {
+      replaceItem = checkboxList[i].parentNode.parentNode;
+      dataItemsTodo.append(replaceItem);
+      numDoList += 1;
+      spanNumDoList.innerHTML = numDoList;
+      numCompleteList -= 1;
+      spanNumCompleteList.innerHTML = numCompleteList;
+    }
+  }
+}
+
+// function replace() {
+//   console.log(this.parentNode);
+// }
